08a1da0e53d10ebedf5666e58af5ed3a7ee4986f
Dylan <dylansoriq@gmail.com>
2026-06-16 02:23:05 -0700

    Initial Commit

---
+++ StudioTests/StudioTests.swift
@@ -0,0 +1,19 @@
+//
+//  StudioTests.swift
+//  StudioTests
+//
+//  Created by Dylan S on 6/16/26.
+//
+
+import Testing
+@testable import Studio
+
+struct StudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
